d8a972582405583461742a52ba0745f7d6d56054
Buezo2019 <nunez_darwin@unah.hn>
2019-02-10 19:40:47 -0600

    Agregamos una lista de productos

---
+++ quieropizza/quieropizza.Web/Controllers/ProductosController.cs
@@ -0,0 +1,46 @@
+﻿using quieropizza.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace quieropizza.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+
+            var producto1 = new ProductoModel();
+            producto1.ID = 1;
+            producto1.Descripcion = "Pizza 6 quesos";
+
+            var producto2 = new ProductoModel();
+            producto2.ID = 2;
+            producto2.Descripcion = "Pizza 4 estaciones";
+
+            var producto3 = new ProductoModel();
+            producto3.ID = 3;
+            producto3.Descripcion = "Pizza Orilla de Queso";
+
+            var producto4 = new ProductoModel();
+            producto4.ID = 4;
+            producto4.Descripcion = "Pizza Suprema";
+
+
+
+            var listadoproductos = new List<ProductoModel>();
+            listadoproductos.Add(producto1);
+            listadoproductos.Add(producto2);
+            listadoproductos.Add(producto3);
+            listadoproductos.Add(producto4);
+            return View(listadoproductos);
+
+
+
+
+        }
+    }
+}
---
+++ quieropizza/quieropizza.Web/Models/ProductoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace quieropizza.Web.Models
+{
+    public class ProductoModel
+    {
+        public int ID { get; set; }
+        public string Descripcion { get; set; }
+
+
+
+
+    }
+}
